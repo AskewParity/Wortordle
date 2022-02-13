39083d344bdbcb45d44feedce1af9c80d6ea9d40
Christopher Lee <chris@Christophers-MacBook-Pro.local>
2022-02-12 22:45:40 -0600

    utilizes word list now

--- temp_tools.py
+++ temp_tools.py
@@ -2,13 +2,15 @@
 
 def js_word_arr() :
     string = '['
-    with open('init_w.txt', 'r', encoding='UTF-8') as o_weights :
+    with open('test.txt', 'r', encoding='UTF-8') as o_weights :
         content = o_weights.readlines()
         for line in content :
             relation = line.strip().split(" ")
-            string += f"['{relation[0]}', {relation[1]}, {relation[2]}], "
+            string += f"['{relation[0]}', {relation[1]}], "
     string = string[0:len(string) - 2] + ']'
-    return string
+    
+    with open ('no_weights.txt', 'w') as file :
+        file.write(string)
 
 if __name__ == "__main__":
     print(js_word_arr())
